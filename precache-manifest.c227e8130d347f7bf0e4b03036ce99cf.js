f5bfdec414827697dd232fa3435de88953c30448
Daniele Barell <dan.barell@gmail.com>
2020-10-12 10:03:30 +0200

    Commit First Test

---
+++ precache-manifest.c227e8130d347f7bf0e4b03036ce99cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efeff5d73935ae54aeae84063a14bd90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c09aad4bbfe1c2af2a3",
+    "url": "/static/css/main.0b1178bd.chunk.css"
+  },
+  {
+    "revision": "c7cf21421388d5eb6454",
+    "url": "/static/js/2.30aaeef0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30aaeef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c09aad4bbfe1c2af2a3",
+    "url": "/static/js/main.0c07d6a9.chunk.js"
+  },
+  {
+    "revision": "8e166a26fdb9f319e8f2",
+    "url": "/static/js/runtime-main.9d3f0a7d.js"
+  }
+]);
